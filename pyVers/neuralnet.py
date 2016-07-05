2cf607e6b2fd1a8dfe56eaba5825cc8035111939
m0baxter <m0baxter@gmail.com>
2016-07-05 15:44:33 -0400

    Simplified (one hidden layer) neural-net added.
    -Skeleton of two new AIs added.
    -Neural network class needs to be bug checked.

--- pyVers/neuralnet.py
+++ pyVers/neuralnet.py
@@ -10,6 +10,11 @@ class NeuralNEt(object):
         self.shape = [inputs] + list(hiddens) + [outputs]
         self.layers = len(self.shape)
         self.weights = self.__randInitialize()
+
+    def numLabels(self):
+        """Returns the number labels in the output of the network."""
+
+        return self.shape[-1]
     
     def __randInitialize(self):
         """Randomly initializes the weight matrices."""
@@ -42,7 +47,78 @@ class NeuralNEt(object):
             start = end
 
         return matrices
- 
+
+    def __labelMatrix(self, y):
+        """converts the label data into a matrix of zeroes and ones."""
+
+        m, n = y.shape
+        Y = np.zeros(i (m, self.numLabels()) )
+
+        for i in xrange(m):
+            Y[i,:] = np.eye(nl)[ y[i] % nl, : ]
+
+        return Y
+
+    def __nnCost(self, theta, X, Y, l):
+        """Computes the cost and its gradient for the neural network on data X, Y
+           with regularizer l."""
+
+        m, n = X.shape
+
+        Ws = self.__unflatten(theta)
+
+        A1 = np.append( np.ones((m,1)), X, 1)
+        Z2 = A1.dot(Ws[0].T)
+        mz, nz = Z2.shape
+        A2 = np.append( np.ones((mz,1)), sigmoid(Z2), 1)
+        Z3 = A2.dot(Ws[1].T)
+        A3 = sigmoid(Z3)
+
+        #compute cost:
+        reg = l*np.sum( [ np.sum( w[:,1:]**2 ) for w in Ws ] )/(2.0 * m)
+        cost = -np.sum( np.log(As[-1])*Y + np.log(1 - As[-1] + 1.0e-15)*(1 - T))/m + reg
+
+        #gradient:
+        D3 = A3 - Y
+        D2 = (D3.dot(Ws[1]) * np.append( np.ones((m,1)), sigGrad(z2)) )[:,1:end]
+
+        Delta2 = D3.T.dot(A2)
+        Delta1 = D2.T.dot(A1)
+
+        mw1, _ = Ws[0].shape
+        mw2, _ = Ws[1].shape
+
+        grad1 =  ( Delta1 + l*np.append(np.zeros((mw1,1)), Ws[0][:,1:], 1) )/m
+        grad2 =  ( Delta1 + l*np.append(np.zeros((mw2,1)), Ws[1][:,1:], 1) )/m
+
+        return ( np.asscalar(cost), np.append(grad1.flatten(), grad2.flatten()) )
+
+
+    def trainNetwork(self, X, y, l):
+        """Trains the neural network from the data X with labels y and regularizer l."""
+        
+        Y = self.__labelMatrix(y)
+        theta = np.concatenate([w.flatten() for w in self.weights])
+
+        res = minimize( self.__nnCost, theta, args=(X,Y,l), method='CG',
+                        jac =True, options={'disp': True})
+
+        self.weights = self.__unflatten(res.x)
+
+        return
+
+    def evaluate(self, x):
+        """Evaluates the neural network on a single example x, a 1xn vector."""
+
+        a1 = np.append( np.ones((1,1)), x, 1)
+        z2 = a1.dot(self.weights[0].T)
+        mz, nz = z2.shape
+        a2 = np.append( np.ones((mz,1)), sigmoid(z2), 1)
+        z3 = a2.dot(self.weights[1].T)
+        a3 = sigmoid(z3)
+
+        return a3.argmax()
+
 
 def randMatrix(size, eps):
     """Returns random matrix with shape = size whose values range in [-eps,eps]."""
@@ -56,17 +132,17 @@ def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
 
+def sigGrad(z):
+    """Returns the gradient of the sigmoid at z (saclar or numpy array)."""
+
+    s = sigmoid(z)
+    return s * (1 - s)
+
+
 if __name__ == "__main__":
 
-    nn = NeuralNEt(9, 9, 5, 6)
-    w = nn.weights
+    nn = NeuralNEt(9, 3, 5)
 
-    flat = np.append(w[0].flatten(), w[1].flatten())
-    flat = np.append(flat, w[2].flatten())
-
-    recw = nn.unflatten(flat)
-
-    print w[0] - recw[0]
-    print w[1] - recw[1]
-    print w[2] - recw[2]
+    x = np.array([[1,2,3,4,5,6,7,8,9]])
+    print nn.evaluate(x)
 
--- pyVers/tttAI.py
+++ pyVers/tttAI.py
@@ -1,6 +1,7 @@
 
 import abc
 import random as rnd
+import numpy as np
 
 
 class TicTacToeAI(object):
@@ -19,4 +20,33 @@ class RandomAI(TicTacToeAI):
       """Randomly places a marker in a square."""
 
       return rnd.randrange(0,9)
+
+
+class HumanPlayer(TicTacToeAI):
+
+   def takeTurn(self, board):
+      """Takes one turn."""
+      
+      #fill in later
+      pass
+
+
+class NNAI(TicTacToeAI):
+
+   def takeTurn(self, board):
+      """Takes one turn."""
+      
+      #fill in later
+      pass
+
+
+def readin( path, DT = float ):
+
+    data = np.loadtxt( path, delimiter = " ", dtype=DT)
+    X = data[:, 0:9]
+    m,n = X.shape
+    print m,n
+    y = data[:,9].reshape((m,1))
+
+    return (X, y)
 
